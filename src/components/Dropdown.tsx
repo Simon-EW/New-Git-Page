f92a519a8596278e82bcaeb583dadeea1f974ea0
Simon EW <simon.ew.ew@gmail.com>
2023-05-26 22:45:07 +0200

    Dropdown is now dropdowning

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import dropdownArrow from "@/icons/DropdownArrow.svg";
+import style from "./dropdown.module.css";
+
+export default function Dropdown({
+  label,
+  items,
+}: {
+  label: string;
+  items: { label: string; link: string }[];
+}) {
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  const button = useRef<HTMLButtonElement>(null);
+  const dropdown = useRef<HTMLUListElement>(null);
+
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const onClickOutside = (e: MouseEvent) => {
+      if (button.current && dropdown.current) {
+        if (
+          !button.current.contains(e.target as Node) &&
+          !dropdown.current.contains(e.target as Node)
+        ) {
+          setClicked(false);
+          setHovered(false);
+        }
+      }
+    };
+
+    document.addEventListener("click", onClickOutside);
+
+    return () => {
+      document.removeEventListener("click", onClickOutside);
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+      }
+    };
+  }, []);
+
+  const onClick = () => {
+    if (clicked) {
+      setClicked(false);
+      setHovered(false);
+    } else {
+      setClicked(true);
+    }
+  };
+
+  const onMouseEnter = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    setHovered(true);
+  };
+
+  const onMouseLeave = () => {
+    timeout.current = setTimeout(() => {
+      setHovered(false);
+    }, 150);
+  };
+
+  return (
+    <>
+      <div className={style.dropdown}>
+        <button
+          aria-expanded="false"
+          aria-label={label}
+          onClick={onClick}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+          ref={button}
+          className={style.button}
+        >
+          <span>{label}</span>
+          <Image
+            src={dropdownArrow}
+            alt=""
+            aria-hidden
+            className={hovered || clicked ? style.flip : ""}
+          />
+        </button>
+        <ul
+          aria-labelledby={label}
+          aria-expanded={hovered || clicked}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+          ref={dropdown}
+          className={`${style.items}`}
+        >
+          {items.map((child) => (
+            <li key={label}>
+              <a href={child.link}>{child.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,8 @@
 import style from "./nav.module.css";
 import Image from "next/image";
 import gitIcon from "@/icons/Git.svg";
-import dropdownArrow from "@/icons/DropdownArrow.svg";
-import { useRef, useState } from "react";
+
+import Dropdown from "./Dropdown";
 
 export default function Nav() {
   return (
@@ -18,61 +18,13 @@ export default function Nav() {
           <a href="/contact">Contact</a>
         </li>
         <li>
-          <button aria-expanded="false">
-            <span>About</span>
-            <Image src={dropdownArrow} alt="" aria-hidden />
-          </button>
-          <div aria-labelledby="about"></div>
+          <Dropdown
+            label="About"
+            items={[{ label: "Test", link: "google.com" }]}
+          />
         </li>
       </ul>
       <button className={style.download}>Download</button>
     </nav>
-  );
-}
-
-function Dropdown({
-  label,
-  children,
-}: {
-  label: string;
-  children: { label: string; link: string }[];
-}) {
-  const [expanded, setExpanded] = useState(false);
-
-  const button = useRef<HTMLButtonElement>(null);
-  const dropdown = useRef<HTMLDivElement>(null);
-
-  const onClick = () => {
-    setExpanded((state) => !state);
-  };
-
-  const onMouseEnter = () => {
-    setExpanded(true);
-  };
-
-  const onMouseLeave = (e: MouseEvent) => {
-    setExpanded(false);
-  };
-
-  return (
-    <>
-      <button
-        aria-expanded="false"
-        aria-label={label}
-        onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        <span>{label}</span>
-        <Image src={dropdownArrow} alt="" aria-hidden />
-      </button>
-      <ul aria-labelledby={label}>
-        {children.map((child) => (
-          <li key={label} className={expanded ? "expanded" : ""}>
-            <a href={child.link}>{child.label}</a>
-          </li>
-        ))}
-      </ul>
-    </>
   );
 }

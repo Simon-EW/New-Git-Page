1454bfdeb59ce011f2677a03ca620223f2b549d2
Simon EW <simon.erikssonwestlund@elev.ga.ntig.se>
2023-05-28 12:07:18 +0200

    Carousel look ait

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import { images } from "./images";
 import style from "./carousel.module.css";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Carousel() {
-  console.log(images[0]);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((currentImage) => (currentImage + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={style.carousel}>
       {images.map((image, index) => (
-        <div key={index} className={style.item}>
+        <a
+          key={index}
+          className={`${style.item} ${
+            index === currentImage ? style.current : ""
+          }`}
+          href={image.href}
+          target="_blank"
+        >
           <Image src={image.src} alt={image.alt} />
-        </div>
+        </a>
       ))}
     </div>
   );
--- src/components/carousel/images.ts
+++ src/components/carousel/images.ts
@@ -1,25 +1,47 @@
-import Android from "@/icons/carousel/Android.svg"
-import Eclipse from "@/icons/carousel/Eclipse.svg"
-import Gnome from "@/icons/carousel/Gnome.svg"
-import Google from "@/icons/carousel/Google.svg"
-import Linkedin from "@/icons/carousel/Linkedin.svg"
-import Microsoft from "@/icons/carousel/Microsoft.svg"
-import Netflix from "@/icons/carousel/Netflix.svg"
-import Perl from "@/icons/carousel/Perl.svg"
-import Postgres from "@/icons/carousel/Postgres.svg"
-import Ruby from "@/icons/carousel/Ruby.svg"
-import Xorg from "@/icons/carousel/Xorg.svg"
+import Android from "@/icons/carousel/Android.svg";
+import Eclipse from "@/icons/carousel/Eclipse.svg";
+import Gnome from "@/icons/carousel/Gnome.svg";
+import Google from "@/icons/carousel/Google.svg";
+import Linkedin from "@/icons/carousel/Linkedin.svg";
+import Linux from "@/icons/carousel/Linux.svg";
+import Microsoft from "@/icons/carousel/Microsoft.svg";
+import Netflix from "@/icons/carousel/Netflix.svg";
+import Perl from "@/icons/carousel/Perl.svg";
+import Postgres from "@/icons/carousel/Postgres.svg";
+import Ruby from "@/icons/carousel/Ruby.svg";
+import Xorg from "@/icons/carousel/Xorg.svg";
 
 export const images = [
-    { src: Android, alt: "Android" },
-    { src: Eclipse, alt: "Eclipse" },
-    { src: Gnome, alt: "Gnome" },
-    { src: Google, alt: "Google" },
-    { src: Linkedin, alt: "Linkedin" },
-    { src: Microsoft, alt: "Microsoft" },
-    { src: Netflix, alt: "Netflix" },
-    { src: Perl, alt: "Perl" },
-    { src: Postgres, alt: "Postgres" },
-    { src: Ruby, alt: "Ruby" },
-    { src: Xorg, alt: "Xorg" },
-]
+  {
+    src: Android,
+    alt: "Android",
+    href: "https://android-review.googlesource.com/#/q/status:open,n,z",
+  },
+  { src: Eclipse, alt: "Eclipse", href: "https://git.eclipse.org/c/" },
+  { src: Gnome, alt: "Gnome", href: "https://www.gnome.org/" },
+  { src: Google, alt: "Google", href: "https://gitlab.gnome.org/GNOME" },
+  { src: Linkedin, alt: "Linkedin", href: "https://www.github.com/linkedin" },
+  {
+    src: Linux,
+    alt: "Linux",
+    href: "https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git",
+  },
+  {
+    src: Microsoft,
+    alt: "Microsoft",
+    href: "https://www.github.com/microsoft",
+  },
+  { src: Netflix, alt: "Netflix", href: "https://www.github.com/netflix" },
+  { src: Perl, alt: "Perl", href: "https://perl5.git.perl.org/perl.git" },
+  {
+    src: Postgres,
+    alt: "Postgres",
+    href: "https://git.postgresql.org/gitweb/",
+  },
+  { src: Ruby, alt: "Ruby", href: "https://github.com/rails/rails" },
+  {
+    src: Xorg,
+    alt: "Xorg",
+    href: "https://cgit.freedesktop.org/xorg/xserver/",
+  },
+];

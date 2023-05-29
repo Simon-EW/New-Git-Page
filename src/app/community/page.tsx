9d35e53acc099bc117bdaec249e64c6f96002011
Simon EW <simon.erikssonwestlund@elev.ga.ntig.se>
2023-05-29 09:09:18 +0200

    Test deployment

---
+++ src/app/community/page.tsx
@@ -0,0 +1,11 @@
+import style from "./page.module.css";
+
+export default function Page() {
+  return (
+    <>
+      <main className={style.main}>
+        <h1>Community</h1>
+      </main>
+    </>
+  );
+}
